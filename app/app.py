bed95419ec17c7f7ba38e9bda8fb953a33e8bdc1
Jethe Krushi <krushi.jethe@gmail.com>
2025-04-05 09:31:54 +0000

    Update app script

--- app/app.py
+++ app/app.py
@@ -1,95 +1,110 @@
+# pylint: disable=import-error
+
+"""
+Docstring
+"""
+
 import io
 import base64
 from PIL import Image
 from flask import Flask, request, render_template
-from text_search import Text_search 
-from visual_search import enhanced_Visual_search
-
-
+from src.product_search_engine import ProductSearchEngine
 
 app = Flask(__name__)
+search_engine = ProductSearchEngine()
 
-Text_obj = Text_search()
-Visual_obj = enhanced_Visual_search()
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
-  
-@app.route('/text_search',methods=['POST'])
+    """
+    Docstring
+
+    Returns:
+        _type_: _description_
+    """
+    return render_template("index.html")
+
+
+@app.route("/text_search", methods=["POST"])
 def text_search():
-    
-    inp_text = str(request.form['input_text'])
-    prediction_text = Text_obj.run(input_text=inp_text)
-    
-    if prediction_text == 'Sorry, we could not find what you are looking for!':
-        
-            return render_template('index.html', prediction_text = prediction_text)
-    else:
-        
-            # Convert the list of PIL images to a list of data URLs
-            data_urls = []
-            for encoding in prediction_text:
-                buffered = io.BytesIO()
-                encoding.save(buffered, format="JPEG")
-                img_str = base64.b64encode(buffered.getvalue()).decode()
-                data_url = f"data:image/jpeg;base64,{img_str}"
-                data_urls.append(data_url)
-            return render_template('index.html', data_urls = data_urls)
-        
-        
-@app.route('/visual_search',methods=['POST'])
+    """
+    Docstring
+
+    Returns:
+        _type_: _description_
+    """
+
+    inp_text = str(request.form["input_text"])
+    search_results = search_engine.text_search(inp_text=inp_text)
+
+    if isinstance(search_results, str):
+        return render_template("index.html", prediction_text=search_results)
+
+    data_urls = gen_data_urls(search_results=search_results)
+    return render_template("index.html", data_urls=data_urls)
+
+
+@app.route("/visual_search", methods=["POST"])
 def visual_search():
-    
-    inp_img = request.files['image_file']
+    """
+    Docstring
+
+    Returns:
+        _type_: _description_
+    """
+
+    inp_img = request.files["image_file"]
     inp_img = Image.open(io.BytesIO(inp_img.read()))
-    
-    prediction_image = Visual_obj.run(inp_img)
-    
-    if prediction_image == 'Sorry, we could not find what you are looking for!':
-        
-            return render_template('index.html', prediction_text = prediction_image)
-    else:
-        
-            # Convert the list of PIL images to a list of data URLs
-            data_urls = []
-            for encoding in prediction_image:
-                buffered = io.BytesIO()
-                encoding.save(buffered, format="JPEG")
-                img_str = base64.b64encode(buffered.getvalue()).decode()
-                data_url = f"data:image/jpeg;base64,{img_str}"
-                data_urls.append(data_url)
-            return render_template('index.html', data_urls = data_urls)
+
+    search_results = search_engine.visual_search(inp_img=inp_img)
+
+    if isinstance(search_results, str):
+        return render_template("index.html", prediction_text=search_results)
+
+    # Convert the list of PIL images to a list of data URLs
+    data_urls = gen_data_urls(search_results=search_results)
+    return render_template("index.html", data_urls=data_urls)
 
 
-@app.route('/audio_search',methods=['POST'])
+@app.route("/audio_search", methods=["POST"])
 def audio_search():
-    
-    inp_text = audio_to_text()
-    if inp_text=="Google Web Speech API could not understand the audio.":
-            return render_template('index.html', prediction_text = inp_text)
-        
-    prediction_text = Text_obj.run(input_text=inp_text)
-    
-    if prediction_text == 'Sorry, we could not find what you are looking for!':
-        
-            return render_template('index.html', prediction_text = prediction_text)
-    else:
-        
-            # Convert the list of PIL images to a list of data URLs
-            data_urls = []
-            for encoding in prediction_text:
-                buffered = io.BytesIO()
-                encoding.save(buffered, format="JPEG")
-                img_str = base64.b64encode(buffered.getvalue()).decode()
-                data_url = f"data:image/jpeg;base64,{img_str}"
-                data_urls.append(data_url)
-            return render_template('index.html', data_urls = data_urls)
-           
+    """
+    Docstring
+
+    Returns:
+        _type_: _description_
+    """
+
+    search_results = search_engine.audio_search()
+
+    if isinstance(search_results, str):
+        return render_template("index.html", prediction_text=search_results)
+
+    data_urls = gen_data_urls(search_results=search_results)
+    return render_template("index.html", data_urls=data_urls)
+
+
+def gen_data_urls(search_results):
+    """
+    Convert the list of PIL images to a list of data URLs
+
+    Args:
+        search_results (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    data_urls = []
+    for encoding in search_results:
+        buffered = io.BytesIO()
+        encoding.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        data_url = f"data:image/jpeg;base64,{img_str}"
+        data_urls.append(data_url)
+
+    return data_urls
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-    
-    
