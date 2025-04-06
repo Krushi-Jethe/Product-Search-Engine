d1d7f4d630a79faffe5e92de1c393e81ea2209fc
Jethe Krushi <krushi.jethe@gmail.com>
2025-04-06 08:52:43 +0000

    update import for ViT

--- src/data_and_models.py
+++ src/data_and_models.py
@@ -8,7 +8,7 @@ import pandas as pd
 from datasets import load_dataset
 from sklearn.neighbors import NearestNeighbors
 from sentence_transformers import SentenceTransformer
-from transformers import ViTFeatureExtractor, ViTForImageClassification
+from transformers import ViTImageProcessor, ViTForImageClassification
 from transformers import ViltProcessor, ViltForImageAndTextRetrieval
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -27,7 +27,7 @@ knn.fit(img_embeddings)
 
 text_encoder = SentenceTransformer("all-MiniLM-L6-v2").to(device)
 
-vit_processor = ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
+vit_processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224")
 vit_model = ViTForImageClassification.from_pretrained(
     "google/vit-base-patch16-224"
 ).to(device)
